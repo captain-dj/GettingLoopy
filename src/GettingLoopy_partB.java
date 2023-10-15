f7f3488aaca23482c2579a16186991f947337cc1
captain-dj <whardig10@gmail.com>
2023-10-15 18:11:36 -0400

    V2.2

--- src/GettingLoopy_partB.java
+++ src/GettingLoopy_partB.java
@@ -2,7 +2,7 @@ public class GettingLoopy_partB {
     public static void main(String[] args) {
         int rows = 5;
         for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
+            for (int j = 1; j <= rows - i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
