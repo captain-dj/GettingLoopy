22d41a543d1d4be02b0113cf2b37cd22f48ffd57
captain-dj <whardig10@gmail.com>
2023-10-15 18:02:06 -0400

    V1.3

--- src/GettingLoopy.java
+++ src/GettingLoopy.java
@@ -1,7 +1,7 @@
 public class GettingLoopy {
     public static void main(String[] args) {
-        for (int i = 30; i >= 0; i--) {
+        for (int i = 0; i <= 18; i += 3) {
             System.out.println(i);
         }
-        }
+    }
     }
